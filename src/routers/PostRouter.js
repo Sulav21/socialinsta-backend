d2d37c5ce9fefa7f70c1b87e4a3a82841025aa76
Sulav Adhikari <sulavadhikari17@gmail.com>
2023-01-17 20:03:45 +1100

    post delete functionality

--- src/routers/PostRouter.js
+++ src/routers/PostRouter.js
@@ -1,76 +1,104 @@
-import express from 'express'
-import { getPostById, insertPost, updatePost } from '../models/post/Post.Model.js'
+import express from "express";
+import {
+  deletePost,
+  getPostById,
+  insertPost,
+  updatePost,
+} from "../models/post/Post.Model.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/',async(req,res,next)=>{
-    try {
-        const result = await insertPost(req.body)
-        if(result?._id){
-            res.json({
-                status:"success",
-                message:"Post inserted successfully"
-            })
-        }else{
-            res.json({
-            status:"error",
-            message:"Couldn't add post"
-            })
-        }
-    } catch (error) {
-        next(error)
+router.post("/", async (req, res, next) => {
+  try {
+    const result = await insertPost(req.body);
+    if (result?._id) {
+      res.json({
+        status: "success",
+        message: "Post inserted successfully",
+      });
+    } else {
+      res.json({
+        status: "error",
+        message: "Couldn't add post",
+      });
     }
-})
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.get("/:_id",async(req,res,next)=>{
-    try {
-          const {_id} = req.params
-          const result  = await getPostById(_id)
-          if(result?._id){
-            res.status(200).json({
-                status:"success",
-                result
-            })
-          }else{
-            res.status(403).json({
-            status:"error",
-            message:"Post doesn't exist, try again later"
-            })
-          }
-    } catch (error) {
-        next(error)
+router.get("/:_id", async (req, res, next) => {
+  try {
+    const { _id } = req.params;
+    const result = await getPostById(_id);
+    if (result?._id) {
+      res.status(200).json({
+        status: "success",
+        result,
+      });
+    } else {
+      res.status(403).json({
+        status: "error",
+        message: "Post doesn't exist, try again later",
+      });
     }
-})
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.put("/:_id", async(req,res,next)=>{
-    try {
-        const result = await getPostById(req.params._id)
-        const {userId, ...rest} = req.body
-        if(result.userId===userId){
-            const updatedPost = await updatePost(result._id,rest)
-            if(updatedPost?._id){
-                res.json({
-                    status:"success",
-                    message:"Post updated successfully",
-                    updatedPost
-                })
-            }else{
-                res.json({
-                    status:"error",
-                    message:"Couldn't update post"
-                })
-            }
-        }else{
-         res.json({
-            status:"error",
-            message:"Post not found !"
-         })
-        }
-
-        
-    } catch (error) {
-        next(error)
+router.put("/:_id", async (req, res, next) => {
+  try {
+    const result = await getPostById(req.params._id);
+    const { userId, ...rest } = req.body;
+    if (result.userId === userId) {
+      const updatedPost = await updatePost(result._id, rest);
+      if (updatedPost?._id) {
+        res.json({
+          status: "success",
+          message: "Post updated successfully",
+          updatedPost,
+        });
+      } else {
+        res.json({
+          status: "error",
+          message: "Couldn't update post",
+        });
+      }
+    } else {
+      res.json({
+        status: "error",
+        message: "Post not found !",
+      });
     }
-})
+  } catch (error) {
+    next(error);
+  }
+});
 
-export default router
+router.delete("/:_id", async (req, res, next) => {
+  try {
+    const { _id } = req.params;
+    const { userId } = req.body;
+    const getPost = await getPostById(_id);
+    if (getPost.userId === userId) {
+      const deletedPost = await deletePost(_id);
+      if (deletedPost?._id) {
+        res.json({
+          status: "success",
+          message: "Post deleted successfully",
+        });
+      }
+      return;
+    } else {
+      res.json({
+        status: "error",
+        message: "Couldn't delete post, try again later",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;
